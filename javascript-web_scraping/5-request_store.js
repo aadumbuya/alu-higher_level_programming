596bde9e1a068dfdbd73d961511c0e4455e38d41
aadumbuya <a.dumbuya@alustudent.com>
2023-07-01 16:22:12 +0000

    javascript-web_scraping

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
-b0VIM 8.1
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+const fs = require('fs');
+const path = process.argv[3];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
